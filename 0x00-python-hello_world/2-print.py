716595171c2372ecd459804eecf81be1b794f5ad
Maryanne Muthoni <maryanne.muthoni@student.moringaschool.com>
2024-03-13 04:37:27 -0700

    Python script that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
    Use the function print

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 # File: 2-print.py
 # Prints "Programming is like building a multilingual puzzle'''
-print("\"Programming is like building a multilingual puzzle")
+print('"Programming is like building a multilingual puzzle')
