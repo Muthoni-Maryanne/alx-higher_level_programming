d012e4a0a4b399f228f79d1cb8f918c8f0fa85a5
Maryanne Muthoni <107298263+Muthoni-Maryanne@users.noreply.github.com>
2024-03-19 05:25:41 +0300

    function that replaces an element in a list at a specific position without modifying the original list
    Prototype: def new_in_list(my_list, idx, element):
    If idx is negative, the function should return a copy of the original list
    If idx is out of range (> of number of element in my_list), the function should return a copy of the original list
    You are not allowed to import any module
    You are not allowed to use try/except

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,10 +3,8 @@ def new_in_list(my_list, idx, element):
     '''replaces an element in a list at a specific position
 
     without modifying the original list'''
-    if idx < 0:
+    if idx < 0 or idx > (len(my_list) - 1):
         return my_listi.copy()
-    elif idx > (len(my_list) - 1):
-        return my_list.copy()
     else:
         list = []
         for number in my_list:
