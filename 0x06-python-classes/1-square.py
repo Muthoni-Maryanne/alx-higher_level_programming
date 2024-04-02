9ba691f5e8114b7b80632ab51ea404bf41493fe6
Maryanne Muthoni <107298263+Muthoni-Maryanne@users.noreply.github.com>
2024-04-02 19:34:38 +0300

    class Square that defines a square by: (based on 0-square.py)
    Private instance attribute: size
    Instantiation with size (no type/value verification)
    You are not allowed to import any module
    Why?

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-'''class Square that defines a square'''
+"""Defines class Square"""
 
 
 class Square:
+    """__init__ method to initialize attributes"""
+
     def __init__(self, size):
+        """private object size attribute"""
         self.__size = size
