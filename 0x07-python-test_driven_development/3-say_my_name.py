39d473cbd6696c5e1719b8d2bf97f281fd0492bc
Maryanne Muthoni <107298263+Muthoni-Maryanne@users.noreply.github.com>
2024-04-13 05:02:09 +0300

    function that prints My name is <first name> <last name>
    Prototype: def say_my_name(first_name, last_name=""):
    first_name and last_name must be strings otherwise, raise a TypeError exception with the message first_name must be a string or last_name must be a string
    You are not allowed to import any module

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,12 +2,9 @@
 
 def say_my_name(first_name, last_name=""):
     ''' prints My name is <first name> <last name>'''
-    try:
-        if isinstance(first_name, str) and isinstance(last_name, str):
-            print("My name is {:s} {:s}".format(first_name, last_name))
-        elif not isinstance(first_name):
-            raise TypeError("first_name must be a string")
-        elif not isinstance(last_name):
-            raise TypeError("last_name must be a string")
-    except TypeError as t:
-        return (t)
+    if isinstance(first_name, str) and isinstance(last_name, str):
+        print("My name is {} {}".format(first_name, last_name))
+    elif not isinstance(first_name):
+        raise TypeError("first_name must be a string")
+    elif not isinstance(last_name):
+        raise TypeError("last_name must be a string")
