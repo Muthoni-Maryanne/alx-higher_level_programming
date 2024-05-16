acc5e3c660f63d0d5c22ec987529cd34e8590f25
Maryanne Muthoni <maryanne.muthoni@moringaschool.com>
2024-05-15 22:47:31 -0700

    class Square that defines a square by: (based on 4-square.py)
    Private instance attribute: size:
    property def size(self): to retrieve it
    property setter def size(self, value): to set it:
    size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
    if size is less than 0, raise a ValueError exception with the message size must be >= 0
    Instantiation with optional size: def __init__(self, size=0):
    Public instance method: def area(self): that returns the current square area
    Public instance method: def my_print(self): that prints in stdout the square with the character #:
    if size is equal to 0, print an empty line
    You are not allowed to import any module

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,36 +3,66 @@
 
 
 class Square:
-    """__init__ method to initialize attributes"""
+    """class Square with one instance attribute and __init__ method"""
 
     def __init__(self, size=0):
-        """private object size attribute"""
-        self.__size = size
+        """Initializes size attribute
 
-        if type(self.__size) is not int:
+        Args:
+            size (int): The size of sides of the Square
+
+        Attributes:
+            size (int): Private instance attribute
+
+        Raises:
+            ValueError: if size is less than 0
+            TypeError: if size is not an int
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
 
-        if self.__size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
+
+        self.__size = size
 
     @property
     def size(self):
+        """int: Getter for the size attribute, returns int
+
+        After is setter method which  modifies size with value::
+            Args:
+                value (int): new value of size
+
+            Attributes:
+                size (int): size of side of Square
+
+            Raises:
+                ValueError: if size is less than 0
+                TypeError: if size is not an int
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        if type(self.__size) is not int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
 
-        if self.__size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
 
         self.__size = value
 
     def area(self):
-        return (self.__size * self.__size)
+        """Method that returns the current square area"""
+        return (self.__size ** 2)
 
     def my_print(self):
+        """ prints in stdout the square with the character #
+
+        Note:
+            if size is equal to 0, print an empty line
+        """
         for i in range(0, self.__size):
             print('#' * self.__size)
         if self.__size == 0:
