9b24be0f20af646f4894d94ca6f8a050b85c7acb
Maryanne Muthoni <107298263+Muthoni-Maryanne@users.noreply.github.com>
2024-05-16 07:53:23 +0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,14 +3,25 @@
 
 
 class Square:
-    """__init__ method to initialize attributes"""
+    """class Square with one instance attribute and __init__ method"""
 
     def __init__(self, size=0):
-        """private object size attribute"""
-        self.__size = size
+        """Initializes size attribute
 
-        if type(self.__size) is not int:
+        Args:
+            size (int): The size of sides of the Square
+
+        Attributes:
+            size (int): Private instance attribute
+            
+        Raises:
+            ValueError: if size is less than 0
+            TypeError: if size is not an int
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-
-        if self.__size < 0:
+            
+        if size < 0:
             raise ValueError("size must be >= 0")
+            
+        self.__size = size
