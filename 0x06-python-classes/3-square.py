8c66f3180fbfa1a94b1e1b0d5be7b79b2075a892
Maryanne Muthoni <107298263+Muthoni-Maryanne@users.noreply.github.com>
2024-05-16 08:06:17 +0300

    class Square that defines a square
    Private instance attribute: size
    Instantiation with optional size: def __init__(self, size=0):
    size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
    if size is less than 0, raise a ValueError exception with the message size must be >= 0
    Public instance method: def area(self): that returns the current square area
    You are not allowed to import any module

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,18 +2,34 @@
 """Defines class Square"""
 
 
+#!/usr/bin/python3
+"""Defines class Square"""
+
+
 class Square:
-    """__init__ method to initialize attributes"""
+    """class Square with one instance attribute and __init__ method"""
 
     def __init__(self, size=0):
-        """private object size attribute"""
-        self.__size = size
+        """Initializes size attribute
 
-        if type(self.__size) is not int:
+        Args:
+            size (int): The size of sides of the Square
+
+        Attributes:
+            size (int): Private instance attribute
+
+        Raises:
+            ValueError: if size is less than 0
+            TypeError: if size is not an int
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
 
-        if self.__size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
 
+        self.__size = size
+
     def area(self):
+        """Method that returns the current square area"""
         return (self.__size ** 2)
