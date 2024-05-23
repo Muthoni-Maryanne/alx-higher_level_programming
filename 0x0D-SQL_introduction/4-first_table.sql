5d245ab6e508e09aab8b7706a122b43f85346c34
Maryanne Muthoni <muthonimaryannewangare@gmail.com>
2024-05-23 18:20:20 +0300

    script that creates a table called first_table in the current database in your MySQL server.
    first_table description:
    id INT
    name VARCHAR(256)
    The database name will be passed as an argument of the mysql command
    If the table first_table already exists, your script should not fail
    You are not allowed to use the SELECT or SHOW statements

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,5 @@
--- Create a first_table in database
--- Exceute: cat 4-first_table.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+-- creates a table called first_table
+-- Description: id INT, name VARCHAR(256)
+-- If the table first_table already exists, your script should not fail
+-- Not allowed to use the SELECT or SHOW statements
+CREATE TABLE first_table(id INT, name VARCHAR(256);
