0b0aa589c4553a9570c13cb445314c8f9a9b55ad
Maryanne Muthoni <muthonimaryannewangare@gmail.com>
2024-05-24 10:49:06 +0300

    script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
    The result should display:
    the score
    the number of records for this score with the label number
    The list should be sorted by the number of records (descending)
    The database name will be passed as an argument to the mysql command

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,5 @@
--- Select count and grouping
--- Execute: cat 15-groups.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+-- script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0
+-- The result should display:the scorethe number of records for this score with the label number
+-- The list should be sorted by the number of records (descending)
+-- Execute with cat 15-groups.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
