95eff285f15794d140e0fc8cb6c1c61dfd388f99
Maryanne Muthoni <muthonimaryannewangare@gmail.com>
2024-05-24 10:38:12 +0300

    script that removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0 in your MySQL server.
    The database name will be passed as an argument of the mysql command

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,3 +1,4 @@
--- Delete rows
--- Execute: cat 13-change_class.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+-- script that removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0
+-- Execute with cat 13-change_class.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+-- Then show table with cat 10-top_score.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 DELETE FROM second_table WHERE score <= 5;
