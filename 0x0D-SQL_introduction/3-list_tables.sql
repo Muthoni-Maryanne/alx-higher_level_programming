c8492220da86dc1deedb8af4c213474271065c49
Maryanne Muthoni <muthonimaryannewangare@gmail.com>
2024-05-24 09:51:41 +0300

    script that lists all the tables of a database in your MySQL server.
    The database name will be passed as argument of mysql command (in the following example: mysql is the name of the database)

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,4 @@
 -- lists all the tables of a database in your MySQL server
 -- database name will be passed as argument of mysql command
+-- Execute with cat 3-list_tables.sql | mysql -hlocalhost -uroot -p mysql
 SHOW TABLES;
