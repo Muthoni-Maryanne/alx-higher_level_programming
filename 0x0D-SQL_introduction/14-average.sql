3c1d08d35c9b5deb0fea7efb6bb29325714e6c3d
Maryanne Muthoni <muthonimaryannewangare@gmail.com>
2024-05-24 10:41:26 +0300

    script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
    The result column name should be average
    The database name will be passed as an argument of the mysql command

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,4 @@
--- Print the average score
--- Execute: cat 14-average.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-SELECT AVG(score) AS average FROM second_table;
+-- script that computes the score average of all records in the table second_table of the database hbtn_0c_0
+-- The result column name should be average
+-- Execute with cat 14-average.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT AVG(score) as average FROM second_table;
