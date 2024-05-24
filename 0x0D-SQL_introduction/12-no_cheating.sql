60f62de7f3c30236d8cb2efdaa7c6df43cf59130
Maryanne Muthoni <muthonimaryannewangare@gmail.com>
2024-05-24 10:32:59 +0300

    script that updates the score of Bob to 10 in the table second_table.
    You are not allowed to use Bob’s id value, only the name field
    The database name will be passed as an argument of the mysql command

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,5 @@
--- update data
--- Execute: cat 12-no_cheating.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-UPDATE `second_table` SET `score`=10 WHERE `second_table`.`name`="Bob";
+-- script that updates the score of Bob to 10 in the table second_table
+-- Not allowed to use Bob’s id value, only the name field
+-- Execute with cat 12-no_cheating.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+-- Then show table after with cat 10-top_score.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+UPDATE second_table SET score = 10 WHERE name = "Bob";
