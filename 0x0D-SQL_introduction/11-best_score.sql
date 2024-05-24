e1cee51309bac167fd55f2376f41799bde088ab3
Maryanne Muthoni <muthonimaryannewangare@gmail.com>
2024-05-24 10:27:56 +0300

    script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.
    Results should display both the score and the name (in this order)
    Records should be ordered by score (top first)
    The database name will be passed as an argument of the mysql command

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,5 @@
--- List the score greather than or equal to 10
--- Execute: cat 11-best_score.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+-- script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0
+-- Results should display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
+-- Execute with cat 11-best_score.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
