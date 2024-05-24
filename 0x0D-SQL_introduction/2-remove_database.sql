06dcc3b7a89c9653813e839fab1e9d2405fc49b6
Maryanne Muthoni <muthonimaryannewangare@gmail.com>
2024-05-24 09:50:33 +0300

    script that deletes the database hbtn_0c_0 in your MySQL server.
    If the database hbtn_0c_0 doesn’t exist, your script should not fail
    You are not allowed to use the SELECT or SHOW statements

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,4 +1,6 @@
 -- script that deletes the database hbtn_0c_0 in your MySQL server
 -- If the database hbtn_0c_0 doesn’t exist, your script should not fail
 -- Not allowed to use the SELECT or SHOW statements
+-- check if present or not with cat 0-list_databases.sql | mysql -hlocalhost -uroot -p
+-- Execute drop with cat 2-remove_database.sql | mysql -hlocalhost -uroot -p
 DROP DATABASE IF EXISTS hbtn_0c_0;
