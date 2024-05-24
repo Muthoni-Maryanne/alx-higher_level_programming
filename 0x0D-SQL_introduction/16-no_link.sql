06b6d8db39f21a06a94ffa10f19d64d4a4484700
Maryanne Muthoni <muthonimaryannewangare@gmail.com>
2024-05-24 11:00:00 +0300

    script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
    Don’t list rows without a name value
    Results should display the score and the name (in this order)
    Records should be listed by descending score
    The database name will be passed as an argument to the mysql command

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,6 @@
--- Lists rows with name value
--- Execute: cat 16-no_link.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-SELECT score, name FROM second_table WHERE name!='' OR name IS NOT NULL ORDER BY score DESC;
+-- script that lists all records of the table second_table of the database hbtn_0c_0
+-- Don’t list rows without a name value
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score
+-- Execute with cat 16-no_link.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT DISTINCT score, name FROM second_table ORDER BY score DESC;
