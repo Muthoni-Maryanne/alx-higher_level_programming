ec29cd8c78bfffff7a75fa093641e4e605d1be15
Maryanne Muthoni <muthonimaryannewangare@gmail.com>
2024-05-24 09:28:31 +0300

    script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
    All fields should be printed
    The database name will be passed as an argument of the mysql command

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,3 +1,4 @@
--- List all rows in the table
--- Execute: cat 6-list_values.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+-- script that lists all rows of the table first_table from the database hbtn_0c_0
+-- All fields should be printed
+-- Execute with cat 6-list_values.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 SELECT * FROM first_table;
