516c9347cb2a0fdab6b808e2dbfb03cdc81ec7ec
Maryanne Muthoni <muthonimaryannewangare@gmail.com>
2024-05-28 19:32:33 +0300

    script that creates the MySQL server user user_0d_1.
    user_0d_1 should have all privileges on your MySQL server
    The user_0d_1 password should be set to user_0d_1_pwd
    If the user user_0d_1 already exists, your script should not fail

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,12 @@
--- Creates the database hbtn_0c_0.
-CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
+-- script that creates the MySQL server user user_0d_1
+-- user_0d_1 should have all privileges on your MySQL server
+-- user_0d_1 password should be set to user_0d_1_pwd
+-- If the user user_0d_1 already exists, your script should not fail
+-- Execute with cat 1-create_user.sql | mysql -hlocalhost -uroot -p
+CREATE USER
+	IF NOT EXISTS 'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+	ON *.*
+	TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
