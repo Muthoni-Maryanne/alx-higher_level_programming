7d0f6480274eb620d761c6e1ebc7b679e44a461f
Maryanne Muthoni <muthonimaryannewangare@gmail.com>
2024-05-28 20:26:15 +0300

    script that creates the table id_not_null on your MySQL server.
    id_not_null description:
    id INT with the default value 1
    name VARCHAR(256)
    The database name will be passed as an argument of the mysql command
    If the table id_not_null already exists, your script should not fail

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,8 @@
--- Creates a table called first_table with values id and name.
-CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
+-- script that creates the table id_not_null on your MySQL server
+-- id_not_null description: id INT with the default value 1, name VARCHAR(256)
+-- If the table id_not_null already exists, your script should not fail
+-- Execute with cat 4-never_empty.sql | mysql -hlocalhost -uroot -p hbtn_0d_2
+CREATE TABLE
+	IF NOT EXISTS id_not_null(
+		id INT DEFAULT 1 NOT NULL,
+		name VARCHAR(256));
