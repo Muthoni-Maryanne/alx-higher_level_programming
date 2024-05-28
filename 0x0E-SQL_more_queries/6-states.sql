ccdd3853fd5a03281fbd94e7b30da0cc132e93ac
Maryanne Muthoni <muthonimaryannewangare@gmail.com>
2024-05-28 21:08:44 +0300

    script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
    states description:
    id INT unique, auto generated, can’t be null and is a primary key
    name VARCHAR(256) can’t be null
    If the database hbtn_0d_usa already exists, your script should not fail
    If the table states already exists, your script should not fail

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,2 +1,15 @@
--- Lists all rows of the table first_table.
-SELECT * FROM `first_table`;
+-- script that creates the database hbtn_0d_usa
+-- the table states (in the database hbtn_0d_usa)
+-- states description:
+-- id INT unique, auto generated, can’t be null and is a primary key
+-- name VARCHAR(256) can’t be null
+-- If the database hbtn_0d_usa already exists, your script should not fail
+-- If the table states already exists, your script should not fail
+-- Executed with cat 6-states.sql | mysql -hlocalhost -uroot -p
+CREATE DATABASE
+	IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE 
+	IF NOT EXISTS states(
+		id INT PRIMARY KEY UNIQUE AUTO_INCREMENT,
+		name VARCHAR(256) NOT NULL);
