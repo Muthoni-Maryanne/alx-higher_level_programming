570db5041d3af46178fa8fbb8e5a34a44137a2e4
Maryanne Muthoni <muthonimaryannewangare@gmail.com>
2024-05-28 19:52:58 +0300

    script that creates the database hbtn_0d_2 and the user user_0d_2.
    user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
    The user_0d_2 password should be set to user_0d_2_pwd
    If the database hbtn_0d_2 already exists, your script should not fail
    If the user user_0d_2 already exists, your script should not fail

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,2 +1,15 @@
--- Deletes the database hbtn_0c_0.
-DROP DATABASE IF EXISTS `hbtn_0c_0`;
+-- script that creates the database hbtn_0d_2 and the user user_0d_2.
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+-- The user_0d_2 password should be set to user_0d_2_pwd
+-- If the database hbtn_0d_2 already exists, your script should not fail
+-- If the user user_0d_2 already exists, your script should not fail
+-- Execute with cat 2-create_read_user.sql | mysql -hlocalhost -uroot -p
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER
+	IF NOT EXISTS 'user_0d_2'@'localhost'
+	IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+	ON hbtn_0d_2.*
+	TO 'user_0d_2'@'localhost'
+	WITH GRANT OPTION;
+FLUSH PRIVILEGES;
