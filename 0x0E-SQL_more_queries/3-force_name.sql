31005049f6a64c9f954615e275dcbee444c8a832
Maryanne Muthoni <muthonimaryannewangare@gmail.com>
2024-05-28 20:15:05 +0300

    script that creates the table force_name on your MySQL server.
    force_name description:
    id INT
    name VARCHAR(256) can’t be null
    The database name will be passed as an argument of the mysql command
    If the table force_name already exists, your script should not fail

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,8 @@
--- Lists all tables of a database.
-SHOW TABLES;
+-- script that creates the table force_name on your MySQL server
+-- force_name description: id INT, name VARCHAR(256) can’t be null
+-- If the table force_name already exists, your script should not fail
+-- Execute with cat 3-force_name.sql | mysql -hlocalhost -uroot -p hbtn_0d_2
+CREATE TABLE
+	IF NOT EXISTS force_name(
+		id INT,
+		name VARCHAR(256) NOT NULL)
