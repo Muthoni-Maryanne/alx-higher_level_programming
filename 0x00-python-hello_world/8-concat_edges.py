46cf3660ac364059c6f44bbff1f0c3b012034138
MaryanneMuthoni <maryanne.muthoni@student.moringaschool.com>
2025-10-06 11:20:18 -0700

    Complete this source code to print object-oriented programming with Python, followed by a new line.
    source code:
    
    str = "Python is an interpreted, interactive, object-oriented programming\
     language that combines remarkable power with very clear syntax"
    print(str)
    
    You are not allowed to use any loops or conditional statements
    Your program should be exactly 5 lines long
    You are not allowed to create new variables
    You are not allowed to use string literals

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.find('object-oriented programming'):str.find(' language')] + str[str.find(' with'):str.find(' very')] + ' ' + str[0:6]
+str = str[str.find('obj'):str.find(' lang')] + str[str.find(' with'):str.find(' very')] + ' ' + str[0:6]
 print(str)
