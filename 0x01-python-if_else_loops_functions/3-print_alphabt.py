da66c03119444f08740cdba2cca4c6aad3a3706b
MaryanneMuthoni <maryanne.muthoni@student.moringaschool.com>
2025-10-07 01:17:37 -0700

    rite a program that prints the ASCII alphabet, in lowercase, not followed by a new line.
    Print all the letters except q and e
    You can only use one print function with string format
    You can only use one loop in your code
    You are not allowed to store characters in a variable
    You are not allowed to import any module

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# program that prints the ASCII alphabet, in lowercase, not followed by a new line
+# Print all the letters except q and e
+# You can only use one print function with string format
+# You can only use one loop in your code
+# You are not allowed to store characters in a variable
+# You are not allowed to import any module
+
+i = 97
+while i <= 122:
+    if i == 113 or i == 101:
+        i += 1
+        continue
+    
+    print("{:c}".format(i), end="")
+    i += 1
