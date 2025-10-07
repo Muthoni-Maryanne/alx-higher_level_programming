e074de19d9c6cfc45ad425499fa69fd48f5bbdd8
MaryanneMuthoni <maryanne.muthoni@student.moringaschool.com>
2025-10-07 01:29:37 -0700

    Write a program that prints all numbers from 0 to 98 in decimal and in hexadecimal (as in the following example)
    You can only use one print function with string format
    You can only use one loop in your code
    You are not allowed to store numbers or strings in a variable
    You are not allowed to import any module

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+
+# program that prints all numbers from 0 to 98 in decimal and in hexadecimal
+# You can only use one print function with string format
+# You can only use one loop in your code
+# You are not allowed to store numbers or strings in a variable
+# You are not allowed to import any module
+
 for i in range(99):
     print("{:d} = {:#x}".format(i, i))
