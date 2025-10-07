1a184c7203e898cb217e6a0ed267102abb8d1b34
MaryanneMuthoni <maryanne.muthoni@student.moringaschool.com>
2025-10-07 01:20:15 -0700

    Fix pycodestyle errors

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# program that prints the ASCII alphabet, in lowercase, not followed by a new line
+# prints the ASCII alphabet, in lowercase, not followed by a new line
 # Print all the letters except q and e
 # You can only use one print function with string format
 # You can only use one loop in your code
@@ -12,6 +12,6 @@ while i <= 122:
     if i == 113 or i == 101:
         i += 1
         continue
-    
+  
     print("{:c}".format(i), end="")
     i += 1
